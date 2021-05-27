92562b6b605bc9113dffa6419df46528daa017f2
Vincent Casilla <73447863+casillavincent@users.noreply.github.com>
2021-05-27 00:40:21 -0700

    Added basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import React, { ReactNode, ReactElement, useState } from "react";
+// import React, { ReactNode, ReactElement, useState } from "react";
+import Basics from "./components/Basics";
 import Hooks from "./components/Hooks";
 
 // Functional Props
-const TextWithNumber = ({ children }: { children: (num: number) => ReactNode }) => {
-   const [state, setState] = React.useState<number>(1);
-   return (
-      <div>
-         <div>
-            <button
-               onClick={() => {
-                  setState(state + 1);
-               }}
-            >
-               Works
-            </button>
-         </div>
-      </div>
-   );
-};
+// const TextWithNumber = ({ children }: { children: (num: number) => ReactNode }) => {
+//    const [state, setState] = React.useState<number>(1);
+//    return (
+//       <div>
+//          <div>
+//             <button
+//                onClick={() => {
+//                   setState(state + 1);
+//                }}
+//             >
+//                Works
+//             </button>
+//          </div>
+//       </div>
+//    );
+// };
 
 function App() {
    return (
       <div className="App">
          <Hooks />
+         <Basics />
       </div>
    );
 }
